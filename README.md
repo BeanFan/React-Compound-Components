be6b5bb0883cae0f7168e6875ca017b94c8d5cec
fanwanyong <38458515+fanwanyong@users.noreply.github.com>
2018-04-17 17:48:49 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Compound-Components
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import FaAutomobile from "react-icons/lib/fa/automobile";
+import FaBed from "react-icons/lib/fa/bed";
+import FaPlane from "react-icons/lib/fa/plane";
+import FaSpaceShuttle from "react-icons/lib/fa/space-shuttle";
+import * as text from "./text";
+
+class Tabs extends Component {
+  state = {
+    activeIndex: 0
+  };
+
+  selectTabIndex = activeIndex => {
+    this.setState({ activeIndex });
+  };
+
+  renderTabs() {
+    const { data } = this.props;
+    return data.map((tab, index) => {
+      const isActive = this.state.activeIndex === index;
+      return (
+        <div
+          key={index}
+          className={isActive ? "tab active" : "tab"}
+          onClick={() => this.selectTabIndex(index)}
+        >
+          {tab.label}
+        </div>
+      );
+    });
+  }
+
+  renderPanel() {
+    const { data } = this.props;
+    const { activeIndex } = this.state;
+    return <div>{data[activeIndex].content}</div>;
+  }
+
+  render() {
+    return (
+      <div className="Tabs">
+        <div className="tabs">{this.renderTabs()}</div>
+        <div className="panels">{this.renderPanel()}</div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    const tabData = [
+      {
+        label: <FaAutomobile />,
+        content: text.cars
+      },
+      {
+        label: <FaBed />,
+        content: text.hotels
+      },
+      {
+        label: <FaPlane />,
+        content: text.flights
+      },
+      {
+        label: <FaSpaceShuttle />,
+        content: text.space
+      }
+    ];
+    return (
+      <div className="App">
+        <Tabs data={tabData} />
+      </div>
+    );
+  }
+}
+
+export default App;
